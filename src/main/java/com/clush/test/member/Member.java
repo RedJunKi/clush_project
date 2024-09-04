ba91ee24dac4c70242965f49860afc8c1f855855
RedJunKi <bing8821@naver.com>
2024-09-04 14:49:06 +0900

    [FEAT] Member Entity 기초설계 (Spring Security, JWT 사용)

---
+++ src/main/java/com/clush/test/member/Member.java
@@ -0,0 +1,37 @@
+package com.clush.test.member;
+
+import com.clush.test.calendar.CalendarEvent;
+import com.clush.test.common.BaseEntity;
+import com.clush.test.role.Role;
+import com.clush.test.todo.Todo;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "MEMBER_ID")
+    private long id;
+
+    private String email;
+    private String password;
+    private String username;
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CalendarEvent> calendarEvents = new ArrayList<>();
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Todo> todos = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(name = "member_role",
+            joinColumns = @JoinColumn(name = "member_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+
+}
---
+++ src/main/java/com/clush/test/role/Role.java
@@ -0,0 +1,4 @@
+package com.clush.test.role;
+
+public class Role {
+}
